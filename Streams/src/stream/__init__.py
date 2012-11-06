3dce41c747d6d5b99538fbeb59ac889d4fea0137
Raghavan <itsrag@gmail.com>
2012-11-06 01:31:10 -0600

    Making change to return None

--- Streams/src/stream/__init__.py
+++ Streams/src/stream/__init__.py
@@ -84,6 +84,8 @@ class PrimeFactors(Stream):
                 self.stream_send_factors.append(factor)
                 return factor
 
+        return None
+
     def popN(self,num_N):
         n_poped_list = ()
         while num_N:
@@ -93,25 +95,20 @@ class PrimeFactors(Stream):
 
     def prime_factorize(self, n):
         number = math.fabs(n)
-
         while number > 1:
             factor = self.get_next_prime_factor(number)
             self.factors.append(factor)
             number /= factor
-
         if n < -1:
             self.factors[0] = -self.factors[0]
-
         return tuple(self.factors)
 
     def get_next_prime_factor(self, n):
         if n % 2 == 0:
             return 2
-
         for x in range(3, int(math.ceil(math.sqrt(n)) + 1), 2):
             if n % x == 0:
                 return x
-
         return int(n)
 
 
